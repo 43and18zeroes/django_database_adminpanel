c44e1db6a42def17e4369fe4cd8fe5aaec07af37
Christoph <c.wagner.germany@gmail.com>
2024-10-10 16:04:19 +0200

    conf

--- database_testproject/urls.py
+++ database_testproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sales project.
+URL configuration for database_testproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
