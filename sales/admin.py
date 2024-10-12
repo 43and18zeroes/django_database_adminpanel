a1ebb2f16b4275a1799cf3d0c0491fa5e502756f
Christoph <c.wagner.germany@gmail.com>
2024-10-12 15:25:17 +0200

    mod2

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Customer, Bill, Product, Producttype, Order
 
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_filter=["first_name","last_name"]
+    list_display=["last_name", "account"]
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Bill)
+admin.site.register(Product)
+admin.site.register(Producttype)
+admin.site.register(Order)
